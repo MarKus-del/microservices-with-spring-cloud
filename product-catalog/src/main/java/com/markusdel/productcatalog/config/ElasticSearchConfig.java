9c48473d35c2591c4163f421cb736a61e8c4e8ea
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-08-07 15:38:11 -0300

    criando configuração deo elasticsearch

---
+++ product-catalog/src/main/java/com/markusdel/productcatalog/config/ElasticSearchConfig.java
@@ -0,0 +1,24 @@
+package com.markusdel.productcatalog.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "com.markusdel.productcatalog.repository")
+public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+    @Override
+    public RestHighLevelClient elasticsearchClient() {
+        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo("localhost:9200", "localhost:9300")
+                .build();
+        return RestClients.create(clientConfiguration).rest();
+    }
+}
