e6650b3bc13e9ca711e1e2518176b12ba326fc27
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-08-07 17:50:20 -0300

    criando config do redis

--- shopping-cart/src/main/java/com/markusdel/shoppingcart/config/RedisConfig.java
+++ shopping-cart/src/main/java/com/markusdel/shoppingcart/config/RedisConfig.java
@@ -1,2 +1,18 @@
-package com.markusdel.shoppingcart.config;public class RedisConfig {
+package com.markusdel.shoppingcart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("localhost", 6379));
+    }
 }
